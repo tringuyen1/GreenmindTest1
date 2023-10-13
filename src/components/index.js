b2be3bde4384cfdaef50b0b9cf90b7441e2a2e18
tringuyen1 <nguyenhoangtri126@gmail.com>
2023-10-13 15:19:28 +0700

    2

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Pagination } from 'swiper/modules';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
@@ -26,11 +26,25 @@ import facebook from '../Images/Group 11.svg'
 import insta from '../Images/Group 12.svg'
 import linkedln from '../Images/Group 13.svg'
 import menu from "../Images/align-justify.svg"
+import x from "../Images/x.svg"
 
 const GreenMind = () => {
+     const [showIcon, setShowIcon] = useState(false);
+
+     const handleShowMenu = () => {
+          var x = document.getElementById("myLinks");
+          if (x.style.display === "block") {
+               x.style.display = "none";
+               setShowIcon(false);
+          } else {
+               x.style.display = "block";
+               setShowIcon(true);
+          }
+     }
+
      return (
           <div className="green-mind">
-               <section className="header mt-4">
+               <section className="header-menu mt-4">
                     <div className="container">
                          <div className="row">
                               <div className="col">
@@ -65,11 +79,53 @@ const GreenMind = () => {
                                              <img className="" src={require('../Images/FilterRight.png')} alt=""></img>
                                         </li>
                                    </ul>
-                                   <ul className='menu-icon menu'>
-                                        <li>
-                                             <img className="" src={menu} alt=""></img>
-                                        </li>
-                                   </ul>
+                              </div>
+                         </div>
+                    </div>
+               </section>
+
+               <section className='header-menu-mobile'>
+                    <div className='container'>
+                         <div className='row'>
+                              <div className='col'>
+                                   <div className='menu'>
+                                        <div class="topnav">
+                                             <img src={ImageGreenMind} alt="" className='img-green-mind mt-1 active'></img>
+                                             <a href="javascript:void(0);" class="icon" onClick={() => handleShowMenu()}>
+                                                  {
+                                                       !showIcon ? (<img className="" src={menu} alt=""></img>) : (<img className="" src={x} alt=""></img>)
+                                                  }
+
+                                             </a>
+                                        </div>
+                                        <div id="myLinks">
+                                             <ul className="link">
+                                                  <li>
+                                                       <a href="/" className="" alt="">Home</a>
+                                                  </li>
+                                                  <li>
+                                                       <a href="/" className="" alt="">Product</a>
+                                                  </li>
+                                                  <li>
+                                                       <a href="/" className="" alt="">Contact</a>
+                                                  </li>
+                                             </ul>
+                                             <ul className='menu-icon'>
+                                                  <li>
+                                                       <img className="" src={require('../Images/Cart.png')} alt=""></img>
+                                                  </li>
+                                                  <li>
+                                                       <img className="" src={require('../Images/Person.png')} alt=""></img>
+                                                  </li>
+                                                  <li>
+                                                       <img className="" src={require('../Images/Line 1.png')} alt=""></img>
+                                                  </li>
+                                                  <li>
+                                                       <img className="" src={require('../Images/FilterRight.png')} alt=""></img>
+                                                  </li>
+                                             </ul>
+                                        </div>
+                                   </div>
                               </div>
                          </div>
                     </div>
